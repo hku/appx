5284907779abc5a59bcecd79d79a5dccbc32ad25
zhanghe <13920826742@163.com>
2018-06-06 10:11:33 +0800

    init project

---
+++ app/src/main/java/com/chinaso/record/base/BaseEntity.java
@@ -0,0 +1,12 @@
+package com.chinaso.record.base;
+
+import java.io.Serializable;
+
+/**
+ * author: zhanghe
+ * created on: 2018/6/5 10:55
+ * description:
+ */
+
+public class BaseEntity implements Serializable {
+}
